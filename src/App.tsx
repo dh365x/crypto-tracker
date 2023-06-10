b1650e37ce8bede59d5b2785574de88dd71ad1b4
dh365x <dh365x@gmail.com>
2023-06-10 19:37:36 +0900

    Setup, 라우트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Coins from "./components/Coins";
+import Coin from "./components/Coin";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path="/:coinId">
+					<Coin />
+				</Route>
+				<Route path="/">
+					<Coins />
+				</Route>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
