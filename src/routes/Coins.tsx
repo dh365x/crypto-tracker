013dbfa25ff0b162a522d05d214ae56042fb1761
dh365x <dh365x@gmail.com>
2023-06-12 14:56:25 +0900

    Home, API 데이터 요청 & 타입정의

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Mobile, PC } from "../styles/MediaQuery";
 import CoinsMoblie from "./CoinsMoblie";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
 	margin-top: 25px;
@@ -44,12 +45,18 @@ const StatsList = styled.div`
 	}
 `;
 
+const Loader = styled.span`
+	display: block;
+	text-align: center;
+`;
+
 const Table = styled.table`
 	width: 100%;
 	border: 1px solid #e5e5e5;
 	border-radius: 10px;
 	border-spacing: 10px;
 	border-collapse: separate;
+	padding: 0 10px;
 	text-align: center;
 	thead {
 		display: table-call;
@@ -66,11 +73,18 @@ const Table = styled.table`
 		vertical-align: middle;
 		height: 65px;
 	}
+	img {
+		width: 32px;
+	}
 	.leftAlign {
 		text-align: left;
 	}
 	.rightAlign {
 		text-align: right;
+	}
+	.vAlign {
+		display: flex;
+		align-items: center;
 	}
 	.fontStyle {
 		font-weight: 500;
@@ -81,109 +95,121 @@ const Table = styled.table`
 	#name {
 		font-size: 18px;
 		font-weight: 500;
-		span:nth-child(2) {
+		span {
+			margin-left: 10px;
+		}
+		span:nth-last-child(1) {
 			font-weight: 400;
 			color: #8c8c8c;
 		}
 	}
 `;
 
+interface ICoin {
+	id: string;
+	icon: string;
+	name: string;
+	symbol: string;
+	rank: number;
+	price: number;
+	priceBtc: number;
+	volume: number;
+	marketCap: number;
+	availableSupply: number;
+	totalSupply: number;
+	priceChange1h: number;
+	priceChange1d: number;
+	priceChange1w: number;
+}
+
 function Coins() {
-	const coins = [
-		{
-			id: "bitcoin",
-			icon: "https://api.coin-stats.com/api/files/812fde17aea65fbb9f1fd8a478547bde/f3738cc5df5f59afb57111d67d951170_1.png",
-			name: "Bitcoin",
-			symbol: "BTC",
-			rank: 1,
-			price: 6362.74960614,
-			priceBtc: 1,
-			volume: 4514555849.85,
-			marketCap: 110545616313,
-			availableSupply: 17373875,
-			totalSupply: 17373875,
-			priceChange1h: 0.12,
-			priceChange1d: -0.56,
-			priceChange1w: -1.07,
-		},
-		{
-			id: "ethereum",
-			icon: "https://api.coin-stats.com/api/files/812fde17aea65fbb9f1fd8a478547bde/e1259737fa19af705f0207d5b384c37e_1027.png",
-			name: "Ethereum",
-			symbol: "ETH",
-			rank: 2,
-			price: 208.130215868,
-			priceBtc: 0.03282527,
-			volume: 1522373585.13,
-			marketCap: 21473551841,
-			availableSupply: 103173639,
-			totalSupply: 103173639,
-			priceChange1h: 0.04,
-			priceChange1d: -1.44,
-			priceChange1w: -1.89,
-		},
-	];
+	const [coins, setCoins] = useState<ICoin[]>([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					`https://api.coinstats.app/public/v1/coins?skip=0&limit=10`
+				);
+				const json = await response.json();
+				setCoins(json.coins);
+				setLoading(false);
+			} catch (error) {
+				console.log("Error fetching data:", error);
+				setLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
 
 	return (
 		<>
 			<PC>
-				<Wrapper>
-					<Container>
-						<Title>Best Coin Price Tracker on the Market</Title>
-						<Desc>
-							<p>
-								With CoinStats, you can manage all your crypto assets from one
-								interface.
-							</p>
-							<p>
-								The global crypto market cap is ₩1408.3T a 0.54 % increase over
-								the last day.
-							</p>
-						</Desc>
-					</Container>
-					<Container>
-						<StatsList>
-							<div></div>
-							<div></div>
-							<div></div>
-						</StatsList>
-					</Container>
-					<Container>
-						<Table>
-							<thead>
-								<tr>
-									<th>#</th>
-									<th className="leftAlign">Name</th>
-									<th className="rightAlign">Change (24h)</th>
-									<th className="rightAlign">Price</th>
-									<th>Price in BTC</th>
-									<th>Market Cap</th>
-									<th>Volume 24h</th>
-								</tr>
-							</thead>
-							<tbody>
-								{coins.map((coin) => (
-									<tr key={coin.id}>
-										<td id="rank">{coin.rank}</td>
-										<td id="name" className="leftAlign">
-											<span>{coin.name}</span>
-											<span> • {coin.symbol}</span>
-										</td>
-										<td id="percentChange24h" className="rightAlign">
-											<span className="fontStyle">{coin.priceChange1d}</span>
-										</td>
-										<td id="price" className="rightAlign">
-											<span className="fontStyle">₩{coin.price}</span>
-										</td>
-										<td id="bitcoinPrice">{coin.priceBtc}</td>
-										<td id="marketCap">₩{coin.marketCap}</td>
-										<td id="volume24h">₩{coin.volume}</td>
+				{loading ? (
+					<Loader>Loading...</Loader>
+				) : (
+					<Wrapper>
+						<Container>
+							<Title>Best Coin Price Tracker on the Market</Title>
+							<Desc>
+								<p>
+									With CoinStats, you can manage all your crypto assets from one
+									interface.
+								</p>
+								<p>
+									The global crypto market cap is ₩1408.3T a 0.54 % increase
+									over the last day.
+								</p>
+							</Desc>
+						</Container>
+						<Container>
+							<StatsList>
+								<div></div>
+								<div></div>
+								<div></div>
+							</StatsList>
+						</Container>
+						<Container>
+							<Table>
+								<thead>
+									<tr>
+										<th className="leftAlign">#</th>
+										<th className="leftAlign">Name</th>
+										<th className="rightAlign">Change (24h)</th>
+										<th className="rightAlign">Price</th>
+										<th>Price in BTC</th>
+										<th>Market Cap</th>
+										<th>Volume 24h</th>
 									</tr>
-								))}
-							</tbody>
-						</Table>
-					</Container>
-				</Wrapper>
+								</thead>
+								<tbody>
+									{coins.map((coin) => (
+										<tr key={coin.id}>
+											<td id="rank">{coin.rank}</td>
+											<td id="name" className="leftAlign">
+												<div className="vAlign">
+													<img src={`${coin.icon}`} alt="" />
+													<span>{coin.name}</span>
+													<span> • {coin.symbol}</span>
+												</div>
+											</td>
+											<td id="percentChange24h" className="rightAlign">
+												<span className="fontStyle">{coin.priceChange1d}</span>
+											</td>
+											<td id="price" className="rightAlign">
+												<span className="fontStyle">₩{coin.price}</span>
+											</td>
+											<td id="bitcoinPrice">{coin.priceBtc}</td>
+											<td id="marketCap">₩{coin.marketCap}</td>
+											<td id="volume24h">₩{coin.volume}</td>
+										</tr>
+									))}
+								</tbody>
+							</Table>
+						</Container>
+					</Wrapper>
+				)}
 			</PC>
 			<Mobile>
 				<CoinsMoblie />
