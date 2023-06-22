bdc892d3141c4706159f44a9c8abc3e93c366ea1
dh365x <dh365x@gmail.com>
2023-06-22 16:08:39 +0900

    News, API 데이터 타입정의 & 상태등록

--- src/routes/News.tsx
+++ src/routes/News.tsx
@@ -1,6 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface INews {
+	news: [
+		{
+			id: string;
+			feedDate: number;
+			source: string;
+			title: string;
+			isFeatured: boolean;
+			description: string;
+			imgURL: string;
+			link: string;
+			sourceLink: string;
+			shareURL: string;
+			relatedCoins: string[];
+		}
+	];
+}
 
 function News() {
+	const [newsData, setNewsData] = useState<INews>();
+	const [loading, setLoading] = useState(true);
+
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
@@ -8,13 +29,16 @@ function News() {
 					`https://api.coinstats.app/public/v1/news?skip=0&limit=20`
 				);
 				const json = await response.json();
-				console.log(json);
+				setNewsData(json);
 			} catch (error) {
 				console.log("Error fetching data:", error);
 			}
+			setLoading(false);
 		};
 		fetchData();
 	}, []);
+
+	console.log(newsData);
 
 	return <div>News</div>;
 }
