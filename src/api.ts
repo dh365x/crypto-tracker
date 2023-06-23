ad8e79d5bf35fe5139c31bf420a13bf27fd1004d
dh365x <dh365x@gmail.com>
2023-06-23 21:46:06 +0900

    axios, API 요청함수 수정

--- src/api.ts
+++ src/api.ts
@@ -1,23 +1,44 @@
+import axios from "axios";
+
+const baseURL = `https://api.coinstats.app/public/v1`;
+
 export function getCoins() {
-	return fetch(
-		`https://api.coinstats.app/public/v1/coins?skip=0&limit=30`
-	).then((response) => response.json());
+	return axios
+		.get(`${baseURL}/coins`, {
+			params: {
+				skip: 0,
+				limit: 30,
+			},
+		})
+		.then((response) => response.data);
 }
 
 export function getCoin(coinId: string) {
-	return fetch(
-		`https://api.coinstats.app/public/v1/coins/${coinId}?currency=AMD`
-	).then((response) => response.json());
+	return axios
+		.get(`${baseURL}/coins/${coinId}`, {
+			params: { currency: "AMD" },
+		})
+		.then((response) => response.data);
 }
 
 export function getCharts(coinId: string) {
-	return fetch(
-		`https://api.coinstats.app/public/v1/charts?period=1w&coinId=${coinId}`
-	).then((response) => response.json());
+	return axios
+		.get(`${baseURL}/charts`, {
+			params: {
+				period: "1w",
+				coinId: coinId,
+			},
+		})
+		.then((response) => response.data);
 }
 
 export function getNews() {
-	return fetch(`https://api.coinstats.app/public/v1/news?skip=0&limit=15`).then(
-		(response) => response.json()
-	);
+	return axios
+		.get(`${baseURL}/news`, {
+			params: {
+				skip: 0,
+				limit: 15,
+			},
+		})
+		.then((response) => response.data);
 }
